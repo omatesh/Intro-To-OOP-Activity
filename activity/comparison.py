68e6e0604b9dd072bf7cab38c5ec90f5bd11c63e
Olya Mateshov <olyamateshov@gmail.com>
2025-03-27 16:54:06 -0400

    "updates to comparison.py and def summary(self) function"

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,7 @@
+from activity.student import Student
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(claire, samara):
+    if samara.get_num_classes() > claire.get_num_classes():
+        return samara.name
+    else:
+        return claire.name
--- activity/student.py
+++ activity/student.py
@@ -6,10 +6,12 @@ class Student:
     
     def add_class(self, class_name):
         self.enrollment_list.append(class_name)
+        return self.enrollment_list
 
     def get_num_classes(self):
         num_enrollment = len(self.enrollment_list)
         return num_enrollment
     
-    def summary(self, name, grade, num_enrollment):
+    def summary(self):
+        num_enrollment = self.get_num_classes()
         return(f"{self.name} is a {self.grade} enrolled in {num_enrollment} classes")
