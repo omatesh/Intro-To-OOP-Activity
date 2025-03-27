7bdd507d1f6def091afd4d244aa6a0c62db3a2e5
KateM <ekaterinamarantidi@gmail.com>
2025-03-27 11:22:22 -0700

    class Student created at 85%

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,13 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, grade, enrollment_list):
+        self.name = name
+        self.grade = grade
+        self.enrollment_list = enrollment_list
+    
+    def add_class(self, class_name):
+        self.enrollment_list.append(class_name)
+
+    def get_num_classes(self):
+        return len(self.enrollment_list)
+    def summary(self):
+        return(f"{})
