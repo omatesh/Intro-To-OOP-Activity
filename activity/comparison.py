411178664810a34ad3555d332a691c5d932fe427
Olya Mateshov <olyamateshov@gmail.com>
2025-04-03 12:19:31 -0400

    Updated get_student_with_more_classes

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,5 +1,4 @@
 from activity.student import Student
-# add your get_student_with_more_classes function here!
 
 def get_student_with_more_classes(student1, student2):
     if student1.get_num_classes() > student2.get_num_classes():
@@ -8,8 +7,3 @@ def get_student_with_more_classes(student1, student2):
         return student2.name
     
     
-# def get_student_with_more_classes(claire, samara):
-#     if samara.get_num_classes() > claire.get_num_classes():
-#         return samara.name
-#     else:
-#         return claire.name
